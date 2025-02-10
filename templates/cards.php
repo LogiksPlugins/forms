5abee7aa2e197c35aef4689373c05e08995f32be
smartinfologiks <devops@smartinfologiks.com>
2025-02-11 03:30:40 +0530

    Cards Template

---
+++ templates/cards.php
@@ -0,0 +1,67 @@
+<?php
+if(!defined('ROOT')) exit('No direct script access allowed');
+
+if(count($fieldGroups)>1) {
+	$groups=array_keys($fieldGroups);
+
+	$accordionID=$formConfig['formkey'];
+
+	echo '<div id="'.$formConfig["formkey"].'"><form class="form validate '.$formConfig['mode'].' '.($formConfig['simpleform']?"simple-form":"").'" method="POST" enctype="multipart/form-data" data-formkey="'.$formConfig["formkey"].'" data-glink="'.$formConfig['gotolink'].'" data-relink="'.$formConfig['reloadlink'].'" data-clink="'.$formConfig['cancellink'].'">';
+	echo '<div class="panel-group accordion-layout" role="tablist" aria-multiselectable="true">';
+	foreach ($groups as $nx=>$fkey) {
+		$title=toTitle(_ling($fkey));
+		$panelID=md5($fkey);
+		echo '<div class="panel panel-default">';
+
+		if(strlen($title)>0) {
+			echo '<div class="panel-heading" role="tab">';
+			echo '<h4 class="panel-title">';
+			if($nx==0) {
+			echo '<a role="button" data-toggle="collapse" aria-expanded="true" aria-controls="collapse'.$panelID.'" data-parent="#accordion'.$accordionID.'" href="#collapse'.$panelID.'" >'.$title.'</a>';
+			} else {
+			echo '<a role="button" data-toggle="collapse" aria-expanded="false" aria-controls="collapse'.$panelID.'" data-parent="#accordion'.$accordionID.'" href="#collapse'.$panelID.'" >'.$title.'</a>';
+			}
+			echo '</h4>';
+			echo '</div>';
+		}
+
+		if($nx==0) {
+			echo '<div class="panel-collapse collapse in" role="tabpanel">';
+		} else {
+			echo '<div class="panel-collapse collapse in" role="tabpanel">';
+		}
+
+		echo '<div class="panel-body">';
+
+		echo "<div class='row'>";
+		echo getFormFieldset($fieldGroups[$fkey],$formData,$formConfig['dbkey'],$formConfig['mode']);
+		echo "</div>";
+
+		echo '</div>';
+		echo '</div>';
+
+		echo '</div>';
+	}
+	echo '</div>';
+
+	echo '<hr class="hr-normal">';
+	echo '<div class="form-actions form-actions-padding"><div class="text-right">';
+	echo getFormActions($formConfig['actions'],$formConfig);
+	echo '</div></div>';
+
+	echo '</form></div>';
+} else {
+	echo '<div class="formbox"><div id="'.$formConfig["formkey"].'" class="formbox-content">';
+	echo '<form class="form validate '.$formConfig['mode'].' '.($formConfig['simpleform']?"simple-form":"").'" method="POST" enctype="multipart/form-data" data-formkey="'.$formConfig["formkey"].'" data-glink="'.$formConfig['gotolink'].'" data-relink="'.$formConfig['reloadlink'].'" data-clink="'.$formConfig['cancellink'].'">';
+	echo "<div class='row'>";
+	echo getFormFieldset($formConfig['fields'],$formData,$formConfig['dbkey'],$formConfig['mode']);
+	echo "</div>";
+	echo '<hr class="hr-normal">';
+	echo '<div class="form-actions form-actions-padding"><div class="text-right">';
+	echo getFormActions($formConfig['actions'],$formConfig);
+	echo '</div></div>';
+	echo '</form></div></div>';
+}
+echo "<script>if(typeof initFormUI=='function') initFormUI('#{$formConfig["formkey"]}'); else $(function() {initFormUI('#{$formConfig["formkey"]}');});</script>";
+//echo "<script>$(function() {initFormUI()});</script>";
+?>
